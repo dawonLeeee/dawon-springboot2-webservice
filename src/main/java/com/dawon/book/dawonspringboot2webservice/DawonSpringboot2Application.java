a29914967b196b45687f4a6374b66ff4932ed364
dawon <ekdnjs210@gmail.com>
2023-10-14 11:51:39 +0900

    Initial commit

---
+++ src/main/java/com/dawon/book/dawonspringboot2webservice/DawonSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.dawon.book.dawonspringboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawonSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawonSpringboot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dawon/book/dawonspringboot2webservice/DawonSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawon.book.dawonspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawonSpringboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
